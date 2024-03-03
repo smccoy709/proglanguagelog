5d20cb525c823f2c37bd1d348511157c5ee4d721
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-03 17:39:42 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import "./App.css";
 
 import LanguagesForm from "./components/LanguagesForm";
-import FilterButton from "./components/FilterButton";
 import LanguagesWrapper from "./components/LanguagesWrapper";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
-const FILTER_MAP = {
-  All: () => true,
-  Active: (task) => !task.completed,
-  Completed: (task) => task.completed,
-};
-
-const FILTER_NAMES = Object.keys(FILTER_MAP);
-
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [parentTasks, setParentTasks] = useState([]);
-  const [filter, setFilter] = useState("All");
-  const filterList = FILTER_NAMES.map((name) => (
-    <FilterButton
-      key={name}
-      name={name}
-      isPressed={name === filter}
-      setFilter={setFilter}
-    />
-  ));
   const taskList = tasks
-    .filter(FILTER_MAP[filter])
     .map((task) => (
       <LanguagesWrapper
         id={task.id}
@@ -134,15 +115,10 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
- 
-  const tasksNoun = taskList.length !== 1 ? "programming languages" : "programming language";
-  const headingText = `${taskList.length} ${tasksNoun} listed`;
   return (
     <div className="todoapp stack-large">
       <h1>Let's Learn How to Code!</h1>
       <LanguagesForm addTask={addTask} parentTasks={parentTasks} />
-      <div className="filters btn-group stack-exception">{filterList}</div>
-      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
